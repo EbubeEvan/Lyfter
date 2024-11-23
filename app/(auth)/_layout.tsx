e9ee4109815a7dd2d4653458b50ad3925f506d90
evangeline <ebubemmayie@gmail.com>
2024-11-23 16:01:10 +0100

    feat: created on boarding and signup pages

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import "@/styles/global.css";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+export default function Rides() {
+  return (
+    <SafeAreaView>
+      <Text>Rides</Text>
+    </SafeAreaView>
+  );
+}
